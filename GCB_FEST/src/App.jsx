8053786947fa15d5b9bedf56392b560b9d955466
Shahid Ali <shahid68491@gmail.com>
2025-10-23 13:28:27 +0500

    adding new file

---
+++ GCB_FEST/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl'>this is the new project ,will start soon</h1>
+    </div>
+    </>
+  )
+}
